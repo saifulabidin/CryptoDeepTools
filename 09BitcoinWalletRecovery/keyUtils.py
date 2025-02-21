4ad2393cce50747a0eea046a3f973f0f605eb9a4
Saiful Abidin <55196633+saifulabidin@users.noreply.github.com>
2025-02-21 23:39:18 +0700

    Update keyUtils.py
    rewritten to be compatible with Python 3

--- 09BitcoinWalletRecovery/keyUtils.py
+++ 09BitcoinWalletRecovery/keyUtils.py
@@ -11,57 +11,55 @@ import base58
 
 # https://en.bitcoin.it/wiki/Wallet_import_format
 
-def privateKeyToWif(key_hex):    
-    return base58.b58encode_check(0x80, key_hex.decode('hex'))
+def privateKeyToWif(key_hex):
+    # key_hex is a hex string; convert to bytes and encode with base58-check using prefix 0x80
+    return base58.b58encode_check(0x80, bytes.fromhex(key_hex))
 
 
 def wifToPrivateKey(s):
     b = base58.bs58decode_check(s)
-    return b.encode('hex')
+    return b.hex()
 
 
-# Input is a hex-encoded, DER-encoded signature
-
-# Output is a 64-byte hex-encoded signature
-
+# Input is a hex-encoded, DER-encoded signature.
+# Output is a 64-byte hex-encoded signature.
 def derSigToHexSig(s):
-    s, junk = ecdsa.der.remove_sequence(s.decode('hex'))
-    if junk != '':
-        print('JUNK', junk.encode('hex'))
-    assert(junk == '')
-    x, s = ecdsa.der.remove_integer(s)
-    y, s = ecdsa.der.remove_integer(s)
+    # Convert hex string to bytes.
+    s_bytes = bytes.fromhex(s)
+    s_bytes, junk = ecdsa.der.remove_sequence(s_bytes)
+    if junk != b'':
+        print('JUNK', junk.hex())
+    assert(junk == b'')
+    x, s_bytes = ecdsa.der.remove_integer(s_bytes)
+    y, s_bytes = ecdsa.der.remove_integer(s_bytes)
     return ('%064x%064x' % (x, y))
 
 
-# Input is hex string
-
+# Input is a hex string representing the private key.
 def privateKeyToPublicKey(s):
-    sk = ecdsa.SigningKey.from_string(s.decode('hex'), curve=ecdsa.SECP256k1)
+    sk = ecdsa.SigningKey.from_string(bytes.fromhex(s), curve=ecdsa.SECP256k1)
     vk = sk.verifying_key
-    return ('\04' + sk.verifying_key.to_string()).encode('hex')
+    return (b'\04' + vk.to_string()).hex()
 
 
-
-# Input is hex string
-
+# Input is a hex string representing the private key.
 def keyToAddr(s):
     return pubKeyToAddr(privateKeyToPublicKey(s))
 
 
-
 def pubKeyToAddr(s):
+    # s is expected to be a hex string representing the public key.
     ripemd160 = hashlib.new('ripemd160')
-    ripemd160.update(hashlib.sha256(s.decode('hex')).digest())
+    ripemd160.update(hashlib.sha256(bytes.fromhex(s)).digest())
     return base58.b58encode_check(ripemd160.digest())
 
 
-
 def addrHashToScriptPubKey(b58str):
-    assert(len(b58str) == 34)
-    # 76     A9      14 (20 bytes)                                 88             AC
-    return ('76a914' + utils.base58CheckDecode(b58str).encode('hex') + '88ac')
-    
+    assert len(b58str) == 34
+    # The script: OP_DUP (76), OP_HASH160 (a9), push 20 bytes (14 in hex),
+    # then the 20-byte hash, then OP_EQUALVERIFY (88) and OP_CHECKSIG (ac)
+    return '76a914' + utils.base58CheckDecode(b58str).hex() + '88ac'
+
 
 class TestKey(unittest.TestCase):
 
@@ -78,51 +76,63 @@ class TestKey(unittest.TestCase):
         self.assertEqual(a, "16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM")
 
     def test_pairs1(self):
-        #blockchain.info
+        # blockchain.info test case
         wallet_addr = "1EyBEhrriJeghX4iqATQEWDq38Ae8ubBJe"
         wallet_private = "8tnArBrrp4KHVjv8WA6HiX4ev56WDhqGA16XJCHJzhNH"
-        wallet_key = utils.base256encode(utils.base58decode(wallet_private)).encode('hex')
+        wallet_key = utils.base256encode(utils.base58decode(wallet_private)).hex()
         self.assertEqual(keyToAddr(wallet_key), wallet_addr)
-        # can import into multibit
+        # Test with Bitcoin QT WIF
         bitcoin_qt = "5Jhw8B9J9QLaMmcBRfz7x8KkD9gwbNoyBMfWyANqiDwm3FFwgGC"
-        wallet_key = utils.base58CheckDecode(bitcoin_qt).encode('hex')
+        wallet_key = utils.base58CheckDecode(bitcoin_qt).hex()
         self.assertEqual(keyToAddr(wallet_key), wallet_addr)
         wallet_key = "754580de93eea21579441b58e0c9b09f54f6005fc71135f5cfac027394b22caa"
         self.assertEqual(keyToAddr(wallet_key), wallet_addr)
 
     def test_pairs2(self):
-        #http://gobittest.appspot.com/Address
-        # Cannot import into multibit
+        # http://gobittest.appspot.com/Address
         wallet_private = "BB08A897EA1E422F989D36DE8D8186D8406BE25E577FD2A66976BF172325CDC9"
         wallet_addr = "1MZ1nxFpvUgaPYYWaLPkLGAtKjRqcCwbGh"
         self.assertEqual(keyToAddr(wallet_private), wallet_addr)
 
     def test_pairs3(self):
-        # Can import into multibit
-        # http://bitaddress.org
+        # Test using bitaddress.org WIF
         wallet_private = "5J8PhneLEaL9qEPvW5voRgrELeXcmM12B6FbiA9wZAwDMnJMb2L"
         wallet_addr = "1Q2SuNLDXDtda7DPnBTocQWtUg1v4xZMrV"
-        self.assertEqual(keyToAddr(utils.base58CheckDecode(wallet_private).encode('hex')), wallet_addr)
+        self.assertEqual(keyToAddr(utils.base58CheckDecode(wallet_private).hex()), wallet_addr)
 
     def test_der(self):
-        self.assertEqual(ecdsa.der.encode_sequence(
+        expected = "300b020312345602040089abcd"
+        encoded = ecdsa.der.encode_sequence(
             ecdsa.der.encode_integer(0x123456),
-            ecdsa.der.encode_integer(0x89abcd)).encode('hex'),
-                         "300b020312345602040089abcd")
+            ecdsa.der.encode_integer(0x89abcd)
+        ).hex()
+        self.assertEqual(encoded, expected)
 
     def test_derSigToHexSig(self):
         derSig = "304502204c01fee2d724fb2e34930c658f585d49be2f6ac87c126506c0179e6977716093022100faad0afd3ae536cfe11f83afaba9a8914fc0e70d4c6d1495333b2fb3df6e8cae"
-        self.assertEqual("4c01fee2d724fb2e34930c658f585d49be2f6ac87c126506c0179e6977716093faad0afd3ae536cfe11f83afaba9a8914fc0e70d4c6d1495333b2fb3df6e8cae",
-                         derSigToHexSig(derSig))
-        txn =          ("0100000001a97830933769fe33c6155286ffae34db44c6b8783a2d8ca52ebee6414d399ec300000000" + "8a47" +                        "304402202c2e1a746c556546f2c959e92f2d0bd2678274823cc55e11628284e4a13016f80220797e716835f9dbcddb752cd0115a970a022ea6f2d8edafff6e087f928e41baac01" + "41" +   "04392b964e911955ed50e4e368a9476bc3f9dcc134280e15636430eb91145dab739f0d68b82cf33003379d885a0b212ac95e9cddfd2d391807934d25995468bc55" +                      "ffffffff02015f0000000000001976a914c8e90996c7c6080ee06284600c684ed904d14c5c88ac204e000000000000" +                        "1976a914348514b329fda7bd33c7b2336cf7cd1fc9544c0588ac00000000")
-        myTxn_forSig =("0100000001a97830933769fe33c6155286ffae34db44c6b8783a2d8ca52ebee6414d399ec300000000" + "1976a914" + "167c74f7491fe552ce9e1912810a984355b8ee07" + "88ac" +  "ffffffff02015f0000000000001976a914c8e90996c7c6080ee06284600c684ed904d14c5c88ac204e000000000000" +"1976a914348514b329fda7bd33c7b2336cf7cd1fc9544c0588ac00000000" + "01000000")
+        expected = "4c01fee2d724fb2e34930c658f585d49be2f6ac87c126506c0179e6977716093faad0afd3ae536cfe11f83afaba9a8914fc0e70d4c6d1495333b2fb3df6e8cae"
+        self.assertEqual(derSigToHexSig(derSig), expected)
+        
+        txn = ("0100000001a97830933769fe33c6155286ffae34db44c6b8783a2d8ca52ebee6414d399ec300000000" +
+               "8a47" +
+               "304402202c2e1a746c556546f2c959e92f2d0bd2678274823cc55e11628284e4a13016f80220797e716835f9dbcddb752cd0115a970a022ea6f2d8edafff6e087f928e41baac01" +
+               "41" +
+               "04392b964e911955ed50e4e368a9476bc3f9dcc134280e15636430eb91145dab739f0d68b82cf33003379d885a0b212ac95e9cddfd2d391807934d25995468bc55" +
+               "ffffffff02015f0000000000001976a914c8e90996c7c6080ee06284600c684ed904d14c5c88ac204e000000000000" +
+               "1976a914348514b329fda7bd33c7b2336cf7cd1fc9544c0588ac00000000")
+        myTxn_forSig = ("0100000001a97830933769fe33c6155286ffae34db44c6b8783a2d8ca52ebee6414d399ec300000000" +
+                          "1976a914" + "167c74f7491fe552ce9e1912810a984355b8ee07" + "88ac" +
+                          "ffffffff02015f0000000000001976a914c8e90996c7c6080ee06284600c684ed904d14c5c88ac204e000000000000" +
+                          "1976a914348514b329fda7bd33c7b2336cf7cd1fc9544c0588ac00000000" +
+                          "01000000")
         public_key = "04392b964e911955ed50e4e368a9476bc3f9dcc134280e15636430eb91145dab739f0d68b82cf33003379d885a0b212ac95e9cddfd2d391807934d25995468bc55"
-        hashToSign = hashlib.sha256(hashlib.sha256(myTxn_forSig.decode('hex')).digest()).digest().encode('hex')
-        sig_der =       "304402202c2e1a746c556546f2c959e92f2d0bd2678274823cc55e11628284e4a13016f80220797e716835f9dbcddb752cd0115a970a022ea6f2d8edafff6e087f928e41baac01"[:-2]
+        hashToSign = hashlib.sha256(hashlib.sha256(bytes.fromhex(myTxn_forSig)).digest()).digest().hex()
+        # Remove the last byte (hashtype) from the DER signature before conversion.
+        sig_der = "304402202c2e1a746c556546f2c959e92f2d0bd2678274823cc55e11628284e4a13016f80220797e716835f9dbcddb752cd0115a970a022ea6f2d8edafff6e087f928e41baac01"[:-2]
         sig = derSigToHexSig(sig_der)
-        vk = ecdsa.VerifyingKey.from_string(public_key[2:].decode('hex'), curve=ecdsa.SECP256k1)
-        self.assertEquals(vk.verify_digest(sig.decode('hex'), hashToSign.decode('hex')), True)
-        #OP_DUP OP_HASH160 167c74f7491fe552ce9e1912810a984355b8ee07 OP_EQUALVERIFY OP_CHECKSIG
+        vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(public_key[2:]), curve=ecdsa.SECP256k1)
+        self.assertTrue(vk.verify_digest(bytes.fromhex(sig), bytes.fromhex(hashToSign)))
+        # OP_DUP OP_HASH160 167c74f7491fe552ce9e1912810a984355b8ee07 OP_EQUALVERIFY OP_CHECKSIG
 
 if __name__ == '__main__':
     unittest.main()
