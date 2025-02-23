1bf7e4459cee52fbbb393339c926754ffee9b20e
Saiful Abidin <55196633+saifulabidin@users.noreply.github.com>
2025-02-24 04:19:43 +0700

    Update breakECDSA.py
    update compatible for python3

--- 08ReducePrivateKey/breakECDSA.py
+++ 08ReducePrivateKey/breakECDSA.py
@@ -4,18 +4,18 @@ import hashlib
 import txnUtils
 import keyUtils
 
-#tx = ""
+# tx = ""
 tx = "" + sys.argv[1]
 
 m = txnUtils.parseTxn(tx)
 e = txnUtils.getSignableTxn(m)
-z = hashlib.sha256(hashlib.sha256(e.decode('hex')).digest()).digest()
-z1 = z[::-1].encode('hex_codec')
-z = z.encode('hex_codec')
-s = keyUtils.derSigToHexSig(m[1][:-2])
-pub =  m[2]
+z = hashlib.sha256(hashlib.sha256(bytes.fromhex(e)).digest()).digest()
+z1 = z[::-1].hex()
+z = z.hex()
+s = keyUtils.derSigToHexSig(m[1][:-2])  # Hapus bytes.fromhex()
+pub = m[2]
 sigR = s[:64]
 sigS = s[-64:]
 sigZ = z
 
-print ("PUBKEY = " + pub + "")
+print("PUBKEY = " + pub)
